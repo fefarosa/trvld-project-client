81a71981169744bbc99005208115d177fef33f98
Suellen Haubrick <suellen.hf@hotmail.com>
2021-03-08 16:54:05 +0000

    logo change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,17 @@
-import './Navbar.css'
+import "./Navbar.css";
 
 function Navbar(props) {
   return (
     <div>
       <nav className="navbar">
-          <img
-            src="./images/PaperAirplane.png"
-            width="30"
-            height="30"
-            className="navbar-brand d-inline-block align-top flex-wrap"
-            alt=""
-          />
-          TRVLD
+        <img
+          src="https://www.flaticon.com/svg/vstatic/svg/2070/2070752.svg?token=exp=1615219084~hmac=e66d40d9e9a524e7f1f61a77c64c8bb8"
+          width="50"
+          height="50"
+          className="navbar-brand d-inline-block align-top flex-wrap"
+          alt=""
+        />
+        TRVLD
       </nav>
     </div>
   );
